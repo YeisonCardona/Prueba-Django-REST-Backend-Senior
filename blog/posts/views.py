39a07de4b26139a918a7af7772b9b7db3828d692
Yeison Cardona <yencardonaal@unal.edu.co>
2023-05-25 12:46:36 -0500

    Add all views

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,11 +1,32 @@
-from users.permissions import IsAdminOrReadOnly
 from rest_framework import viewsets
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment, Like, Tag
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer, TagSerializer
+from users.permissions import IsAdmin, IsEditor, IsBlogger
 
 
 ########################################################################
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAdmin | IsEditor | IsBlogger]
+
+
+########################################################################
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAdmin | IsEditor | IsBlogger]
+
+
+########################################################################
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAdmin | IsEditor | IsBlogger]
+
+
+########################################################################
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [IsAdmin | IsEditor | IsBlogger]
